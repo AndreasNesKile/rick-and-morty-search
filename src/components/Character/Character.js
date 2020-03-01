e9b0ed1b2a3481108426fda031c7d924a867df4d
Andreas Nes Kile <andrenk@stud.ntnu.no>
2020-03-01 23:40:03 +0100

    Added Character class to CharacterList

--- src/components/Character/Character.js
+++ src/components/Character/Character.js
@@ -1,3 +1,19 @@
 // Write a function component 
 // Simply display a character in the form of a card: https://www.w3schools.com/howto/howto_css_cards.asp
 // Style the card however you like.
+import React from 'react'
+
+
+class Character extends React.Component {
+    render(
+
+            <div class="card">
+  <img src="img_avatar.png" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>John Doe</b></h4>
+    <p>Architect & Engineer</p>
+  </div>
+</div>
+ 
+    )
+}
