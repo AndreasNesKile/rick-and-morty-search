bc6a20791f097ccb3df9d6b80141401e0d43ca07
Andreas Nes Kile <andrenk@stud.ntnu.no>
2020-03-04 23:10:00 +0100

    refactoring

--- src/components/CharacterList/CharacterList.js
+++ src/components/CharacterList/CharacterList.js
@@ -6,103 +6,87 @@ import SearchBar from "../SearchBar/SearchBar";
 import InfiniteScroll from "react-infinite-scroller";
 import axios from "axios";
 // Constant To store API url.
-const API_URL = "https://rickandmortyapi.com/api/character/";
+const API_URL = "https://rickandmortyapi.com/api/character/?page=";
 
 class CharacterList extends React.Component {
-    constructor(props) {
-        super(props);
-        this.onChange = this.onChange.bind(this);
-        this.loadMoreCharacters = this.loadMoreCharacters.bind(this);
-    }
-    // Initialize the State in Class Component.
-    state = {
-        characters: [],
-        searchText: "",
-        characterPage: 1
-    };
+	constructor(props) {
+		super(props);
+		this.onChange = this.onChange.bind(this);
+		this.loadMoreCharacters = this.loadMoreCharacters.bind(this);
+	}
+	// Initialize the State in Class Component.
+	state = {
+		characters: [],
+		searchText: "",
+		characterPage: 1
+	};
 
-    onChange(props) {
-        this.setState({
-            searchText: props
-        });
-    }
+	onChange(props) {
+		this.setState({
+			searchText: props
+		});
+	}
 
-    async loadMoreCharacters() {
-        try {
-            axios
-                .get(`${API_URL}?page=${this.state.characterPage}`)
-                .then(resp => {
-                    this.setState({
-                        characters: [
-                            ...this.state.characters,
-                            ...resp.data.results
-                        ],
-                        characterPage: this.state.characterPage + 1
-                    });
-                });
-        } catch (e) {
-            console.error(e);
-        }
-    }
+	async loadMoreCharacters() {
+		try {
+			axios.get(`${API_URL}${this.state.characterPage}`).then(resp => {
+				this.setState({
+					characters: [
+						...this.state.characters,
+						...resp.data.results
+					],
+					characterPage: this.state.characterPage + 1
+				});
+			});
+		} catch (e) {
+			console.error(e);
+		}
+	}
 
-    // Use ASYNC/AWAIT inside lifecycle method.
-    async componentDidMount() {
-        try {
-            axios
-                .get(`${API_URL}?page=${this.state.characterPage}`)
-                .then(resp => {
-                    this.setState({
-                        characters: [
-                            ...this.state.characters,
-                            ...resp.data.results
-                        ],
-                        characterPage: this.state.characterPage + 1
-                    });
-                });
-        } catch (e) {
-            console.error(e);
-        }
-    }
+	// Use ASYNC/AWAIT inside lifecycle method.
+	async componentDidMount() {
+		this.loadMoreCharacters();
+	}
 
-    // Required render() method in Class Component.
-    render() {
-        // Create an array of JSX to render
-        const characters = this.state.characters
-            .filter(character =>
-                character.name
-                    .toLowerCase()
-                    .includes(this.state.searchText.toLowerCase())
-            )
-            .map(character => {
-                // This should render Character components. - Remember the key.
-                return Character(character);
-            });
+	// Required render() method in Class Component.
+	render() {
+		// Create an array of JSX to render
+		const characters = this.state.characters
+			.filter(character =>
+				character.name
+					.toLowerCase()
+					.includes(this.state.searchText.toLowerCase())
+			)
+			.map(character => {
+				// This should render Character components. - Remember the key.
+				return Character(character);
+			});
 
-        // Render MUST return valid JSX.
+		// Render MUST return valid JSX.
 
-        return (
-            <div>
-                <div className={styles.SearchBar}>
-                    <SearchBar
-                        onChange={this.onChange.bind(this.state.characters)}
-                    />
-                </div>
-                <InfiniteScroll
-                    pageStart={1}
-                    initialLoad={false}
-                    loadMore={this.loadMoreCharacters}
-                    loader={
-                        <div className="loader" key={0}>
-                            Loading ...
-                        </div>
-                    }
-                    hasMore={true || false}
-                >
-                    <div className={styles.CharacterList}>{characters}</div>
-                </InfiniteScroll>
-            </div>
-        );
-    }
+		return (
+			<div>
+				<div className={styles.SearchBar}>
+					<SearchBar
+						onChange={this.onChange.bind(this.state.characters)}
+					/>
+				</div>
+				<InfiniteScroll
+					pageStart={1}
+					initialLoad={false}
+					loadMore={this.loadMoreCharacters}
+					loader={
+						<div className="loader" key={0}>
+							Loading ...
+						</div>
+					}
+					hasMore={true || false}
+				>
+					<div className={styles.CharacterList}>{characters}</div>
+				</InfiniteScroll>
+			</div>
+		);
+	}
 }
 
 export default CharacterList;
